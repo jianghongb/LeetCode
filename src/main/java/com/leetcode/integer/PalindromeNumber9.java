d68e6653ee1b0a0ab17285d9b6e37ba27c2eba1f
jianghongb <smilejiangh@gmail.com>
2020-02-02 20:51:45 +0800

    Update Palindrome number with string solution

--- src/main/java/com/leetcode/integer/PalindromeNumber9.java
+++ src/main/java/com/leetcode/integer/PalindromeNumber9.java
@@ -33,4 +33,17 @@ public class PalindromeNumber9 {
         }
         return x == num || num / 10 == x;
     }
+
+    public boolean isPalindromeString(int x) {
+        if (x < 0) {
+            return false;
+        }
+        String s = String.valueOf(x);
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
