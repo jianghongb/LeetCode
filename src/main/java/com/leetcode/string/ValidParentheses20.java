58a1c1c51fd8010a2b37911cd5331ad4ec7c594a
jianghongb <smilejiangh@gmail.com>
2020-02-20 19:40:38 +0800

    Remove Element 27 & Move Zeroes 283

--- src/main/java/com/leetcode/string/ValidParentheses20.java
+++ src/main/java/com/leetcode/string/ValidParentheses20.java
@@ -1,5 +1,6 @@
 package com.leetcode.string;
 
+import java.util.HashMap;
 import java.util.Stack;
 
 /**
@@ -24,7 +25,7 @@ import java.util.Stack;
  */
 public class ValidParentheses20 {
 
-    public boolean isValid(String s) {
+    public boolean isValid2(String s) {
 
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
@@ -42,9 +43,45 @@ public class ValidParentheses20 {
             }
         }
 
-        if (!stack.isEmpty()) {
-            return false;
+        return stack.isEmpty();
+    }
+
+    // Hash table that takes care of the mappings.
+    private HashMap<Character, Character> mappings;
+
+    // Initialize hash map with mappings. This simply makes the code easier to read.
+    public ValidParentheses20() {
+        this.mappings = new HashMap<Character, Character>();
+        this.mappings.put(')', '(');
+        this.mappings.put('}', '{');
+        this.mappings.put(']', '[');
+    }
+
+    public boolean isValid(String s) {
+
+        // Initialize a stack to be used in the algorithm.
+        Stack<Character> stack = new Stack<Character>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            // If the current character is a closing bracket.
+            if (this.mappings.containsKey(c)) {
+
+                // Get the top element of the stack. If the stack is empty, set a dummy value of '#'
+                char topElement = stack.empty() ? '#' : stack.pop();
+
+                // If the mapping for this bracket doesn't match the stack's top element, return false.
+                if (topElement != this.mappings.get(c)) {
+                    return false;
+                }
+            } else {
+                // If it was an opening bracket, push to the stack.
+                stack.push(c);
+            }
         }
-        return true;
+
+        // If the stack still contains elements, then it is an invalid expression.
+        return stack.isEmpty();
     }
 }
