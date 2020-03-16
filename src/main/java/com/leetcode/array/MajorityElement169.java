b7356db350c5304b47b66483e1a9580b5deb089d
jianghongb <smilejiangh@gmail.com>
2020-03-16 22:04:58 +0800

    Add more solutions for solve this problem

--- src/main/java/com/leetcode/array/MajorityElement169.java
+++ src/main/java/com/leetcode/array/MajorityElement169.java
@@ -2,6 +2,7 @@ package com.leetcode.array;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.IntStream;
 
@@ -17,6 +18,12 @@ import java.util.stream.IntStream;
  */
 public class MajorityElement169 {
 
+    /**
+     * Time complexity : O(nlgn)O(nlgn)O(nlgn)
+     * Sorting the array costs O(nlgn)O(nlgn)O(nlgn) time in Python and Java, so it dominates the overall runtime.
+     * Space complexity : O(1)O(1)O(1) or (O(n)O(n)O(n))
+     * We sorted nums in place here - if that is not allowed, then we must spend linear additional space on a copy of nums and sort the copy instead.
+     */
     public int majorityElement(int[] nums) {
         if (nums == null || nums.length == 0) {
             return 0;
@@ -37,5 +44,15 @@ public class MajorityElement169 {
         Arrays.sort(nums);
 
         return nums[nums.length / 2];
+    }
+
+    public int majorityElementMap(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            if (map.get(nums[i]) > nums.length / 2)
+                return nums[i];
+        }
+        return -1;
     }
 }
