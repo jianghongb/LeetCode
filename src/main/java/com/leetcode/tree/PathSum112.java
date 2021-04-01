319af511fdcc2a58e6f074244003d936a70c9acb
jianghongb <smilejiangh@gmail.com>
2021-04-01 23:18:00 +0800

    Path sum

---
+++ src/main/java/com/leetcode/tree/PathSum112.java
@@ -0,0 +1,86 @@
+package com.leetcode.tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * https://leetcode.com/problems/path-sum/
+ *
+ * Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+ *
+ * A leaf is a node with no children.
+ *
+ *
+ *
+ * Example 1:
+ * 5
+ * / \
+ * 4   8
+ * /   / \
+ * 11  13  4
+ * / \       \
+ * 7   2       1
+ *
+ * Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+ * Output: true
+ *
+ * Example 2:
+ * 1
+ * / \
+ * 2  3
+ * Input: root = [1,2,3], targetSum = 5
+ * Output: false
+ *
+ * Example 3:
+ *
+ * Input: root = [1,2], targetSum = 0
+ * Output: false
+ *
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in the tree is in the range [0, 5000].
+ * -1000 <= Node.val <= 1000
+ * -1000 <= targetSum <= 1000
+ */
+public class PathSum112 {
+
+  /**
+   * BFS 或者DFS实现都可以
+   *
+   * @param root
+   * @param targetSum
+   * @return
+   */
+  public boolean hasPathSum2(TreeNode root, int targetSum) {
+
+    return checkPathSum(root, 0, targetSum);
+  }
+
+  public boolean checkPathSum(TreeNode root, int sum, int targetSum) {
+    if (root == null) {
+      return false;
+    }
+    sum += root.val;
+    if (sum == targetSum && root.left == null && root.right == null)
+      return true;
+    if (root.right == null) {
+      return checkPathSum(root.left, sum, targetSum);
+    }
+    if (root.left == null) {
+      return checkPathSum(root.right, sum, targetSum);
+    }
+    return checkPathSum(root.left, sum, targetSum) || checkPathSum(root.right, sum, targetSum);
+  }
+
+  public boolean hasPathSum(TreeNode root, int targetSum) {
+    if (root == null) {
+      return false;
+    }
+    if (root.left == null && root.right == null) {
+      return targetSum - root.val == 0;
+    }
+    return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+  }
+}
