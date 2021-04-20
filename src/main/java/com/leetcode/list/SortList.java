b3b919a9b044b25e470f21f0c5e6f837f5306903
jianghongb <smilejiangh@gmail.com>
2021-04-20 21:34:09 +0800

    Sort List update

--- src/main/java/com/leetcode/list/SortList.java
+++ src/main/java/com/leetcode/list/SortList.java
@@ -1,8 +1,9 @@
 package com.leetcode.list;
 
-import java.util.List;
 
 /**
+ *
+ * https://www.youtube.com/watch?v=M1TwY0nsTZA 花花酱解析
  * Given the head of a linked list, return the list after sorting it in ascending order.
  *
  * Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
