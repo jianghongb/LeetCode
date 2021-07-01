cb5537fce432c94aadb720f3a56eda6907f22c72
jianghongb <smilejiangh@gmail.com>
2021-07-01 14:07:06 +0800

    update valid parenthese

--- src/main/java/com/leetcode/string/ValidParentheses20.java
+++ src/main/java/com/leetcode/string/ValidParentheses20.java
@@ -84,4 +84,26 @@ public class ValidParentheses20 {
         // If the stack still contains elements, then it is an invalid expression.
         return stack.isEmpty();
     }
+
+    public boolean isValid3(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(int i =0;i< s.length();i++){
+            char c = s.charAt(i);
+            if('(' == c){
+                stack.push(')');
+            }else if('{' == c){
+                stack.push('}');
+            } else if('[' == c){
+                stack.push(']');
+            } else if(stack.isEmpty() || stack.peek() != c){
+                return false;
+            }else{
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
 }
