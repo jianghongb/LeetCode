334fb0a85b09ba92e25c118818c9aea99896fb86
jianghongb <smilejiangh@gmail.com>
2021-07-03 19:16:44 +0800

    update with one wrong method

--- src/main/java/com/leetcode/string/RemoveDuplicatesII1029.java
+++ src/main/java/com/leetcode/string/RemoveDuplicatesII1029.java
@@ -34,8 +34,33 @@ public class RemoveDuplicatesII1029 {
     return sb.reverse().toString();
   }
 
+  // 这个算法实现的有问题：不能解决中间存在间隔的情况，扫描过的结果不会再重新计算
+  public String removeDuplicates2(String s, int k) {
+
+    int len = 0, top = -1;
+    StringBuilder res = new StringBuilder();
+    for (int i = 0; i < s.length(); i++) {
+      char ch = s.charAt(i);
+      if (top >= 0 && res.charAt(top) == ch) {
+        len++;
+        res.append(ch);
+        top++;
+        if (len == k) {
+          res.delete(res.length() - k, res.length());
+          top = res.length() - 1;
+          len -= k;
+        }
+      } else {
+        len = 1;
+        res.append(ch);
+        top++;
+      }
+    }
+    return res.toString();
+  }
+
   @Test
   void test() {
-    System.out.println(removeDuplicates("deeedbbcccbdaa", 3));
+    System.out.println(removeDuplicates2("deeedbbcccbdaa", 3));
   }
 }
